07f6db10286a7767d16d5ea37abd6bb1689df136
Bryan Lai <laizhuoh@mail2.sysu.edu.cn>
2015-03-22 00:44:17 +0800

    Delete BasicNode.java

--- Dyscrabble/src/BasicNode.java
+++
@@ -1,9 +0,0 @@
-
-public class BasicNode {
-	int x;					//x coordinate
-	int y;					//y coordinate
-	int horizontal;			//whether it is filled horizontally
-	int vertical;			//whether it is filled vertically
-	char content;			//the content of the node
-	boolean consider;		//will the node be considered by the waitQueue
-}
